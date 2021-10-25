3e0e92c726a17a6ead5d4f64a166a0f4f8a144d2
HajiDavud <tu201806253@code.edu.az>
2021-10-26 00:35:44 +0400

    DDSS

---
+++ task2.cs
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+                            Online C# Compiler.
+                Code, Compile, Run and Debug C# program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+using System;
+class HelloWorld {
+  static void Main() {
+    int b = 100,i=1;
+    while(i<100){
+        Console.WriteLine(i);
+        i+=2;
+    }
+  }
+}
---
+++ task3.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main() {
+        int i;
+        string c;
+        c = Console.ReadLine();
+        i = int.Parse(c);
+        Console.WriteLine(i*2);
+    }
+}
